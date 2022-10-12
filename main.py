2949fd12693d16f265b8c706b859bc29c935400b
Andras Keszte <kesztea@gmail.com>
2022-10-12 21:07:43 +0200

    Load pool table image

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ class Game:
     def __init__(self):
         pygame.init()
 
-        screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT), 0, 32)
         pygame.display.set_caption('PyGame Pool')
+        self.screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT), 0, 32)
         self.clock = pygame.time.Clock()
+
+        self._load_images()
 
     def run(self):
         while True:
+            self.screen.blit(self.table_img, (0, 0))
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -21,6 +25,9 @@ class Game:
 
             self.clock.tick(c.FPS)
             pygame.display.update()
+
+    def _load_images(self):
+        self.table_img = pygame.image.load('images/table.png').convert_alpha()
 
 
 if __name__ == "__main__":
