1e7fab39759cd67dee20d57c3e6d45c83d5bc3e2
Andras Keszte <kesztea@gmail.com>
2022-10-12 20:08:19 +0200

    Refactor to use OOP

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import sys
 import pygame
 
-pygame.init()
-pygame.display.set_caption('PyGame Pool')
-screen = pygame.display.set_mode((800, 600), 0, 32)
-clock = pygame.time.Clock()
 
-while True:
-    screen.fill((0, 0, 0))
+class Game:
+    def __init__(self):
+        pygame.init()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                sys.exit()
+        screen = pygame.display.set_mode((800, 600), 0, 32)
+        pygame.display.set_caption('PyGame Pool')
+        self.clock = pygame.time.Clock()
 
-    clock.tick(60)
-    pygame.display.update()
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.clock.tick(60)
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
